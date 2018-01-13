08db4509d489f4b7874a399bba772cd3d9013418
Chris Nichols <christni@cisco.com>
2018-01-13 15:17:24 -0800

    Add project files.

---
+++ Focus3.XmlTransforms.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Focus3.XmlTransforms.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
